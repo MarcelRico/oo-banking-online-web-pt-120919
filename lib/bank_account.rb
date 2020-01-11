4bb7073c69ba62efaea2ab9e778f7c0230ac8659
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 06:06:02 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+  
+  attr_reader :name
+  attr_accessor :balance,:status
+
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount_to_deposit)
+    @balance += amount_to_deposit
+  end
+
+  def display_balance
+   "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    (@balance > 0) && (@status == "open")
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender,:receiver,:status,:amount
+  
+  def initialize(from_account,to_account,transfer_amount)
+    @sender = from_account
+    @receiver = to_account
+    @status = "pending"
+    @amount = transfer_amount
+  end
+  
+  def valid?
+    (@amount > 0) && (@sender.valid?) && (@receiver.valid?)
+  end
+  
+  def execute_transaction(sender = @sender, receiver = @receiver, completion_status = "complete")
+    
+    status_is_pending = (@status == "pending")
+    insufficent_funds = (sender.balance - @amount <= 0)
+    accounts_not_closed = (sender.status == "open") && (receiver.status == "open")
+    all_validation_passes = status_is_pending && !insufficent_funds && accounts_not_closed && self.valid?
+    
+    if all_validation_passes
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = completion_status
+    elsif !accounts_not_closed || insufficent_funds
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+    
+  end
+  
+  def reverse_transfer
+    if @status == "complete"
+      @status = "pending"
+      self.execute_transaction(@receiver,@sender,"reversed")
+    end
+  end
+  
 end
