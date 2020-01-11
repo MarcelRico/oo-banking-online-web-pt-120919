f7ba3d5ff0e8fa6e92041d5f61bb582bb496a829
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 05:51:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,10 @@ class Transfer
     (@amount > 0) && (@sender.valid?) && (@receiver.valid?)
   end
   
+  def all_validation_passes?
+    
+  end
+  
   def execute_transaction(sender = @sender, receiver = @receiver)
     
     status_is_pending = (@status == "pending")
@@ -33,22 +37,7 @@ class Transfer
   end
   
   def reverse_transfer(sender = @receiver, receiver = @sender)
-    @status = "pending"
-    status_is_pending = (@status == "pending")
-    insufficent_funds = (sender.balance - @amount <= 0)
-    accounts_not_closed = (sender.status == "open") && (receiver.status == "open")
-    all_validation_passes = status_is_pending && !insufficent_funds && accounts_not_closed && self.valid?
-    result = nil
-    
-    if all_validation_passes
-      sender.balance -= @amount
-      receiver.balance += @amount
-      @status = "complete"
-    elsif !accounts_not_closed || insufficent_funds
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
-    
+  
   end
   
 end
