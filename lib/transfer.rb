5f7177584048c818f9d3b88ad8e50e94dc5b18cb
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 05:22:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,33 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender,:receiver,:status,:amount
+  
+  def initialize(from_account,to_account,transfer_amount)
+    @sender = from_account
+    @receiver = to_account
+    @status = "pending"
+    @amount = transfer_amount
+  end
+  
+  def valid?
+    (@amount > 0) && (@sender.valid?) && (@receiver.valid?)
+  end
+  
+  def execute_transaction
+    
+    status_is_pending = (@status == "pending")
+    insufficent_funds = (@sender.balance <= @amount)
+    accounts_not_closed = (@sender.status == "open") && (@receiver.status == "open")
+    all_validation_passes = status_is_pending && !insufficent_funds && accounts_not_closed
+    
+    
+    if all_validation_passes
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+    
+    
+    
+  end
 end
