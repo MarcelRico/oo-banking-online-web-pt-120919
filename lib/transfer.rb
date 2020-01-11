1002ba63ed161675894a0570e60aefdd7f15c118
Marcel Rico <personal.mr4@gmail.com>
2020-01-11 05:30:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     elsif !accounts_not_closed
-      result = ""
+      result = "Transaction rejected. Please check your account balance."
     end
     
     puts "sender balance: #{@sender.balance}"
